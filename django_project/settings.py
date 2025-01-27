0ee2cfc0c167a3ec9747e5a41144be1bfa201e0b
Tropic.Wang <Nemowang24@gmail.com>
2025-01-26 21:17:53 -0500

    change settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["192.168.1.240", "34.231.220.146", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.240", "localhost", "127.0.0.1"]
 
 
 # Application definition
