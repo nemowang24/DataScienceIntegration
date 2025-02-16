e4b46332b626afbb42ff1f57af0a5f214c172038
Tropic.Wang <Nemowang24@gmail.com>
2025-02-15 22:04:16 -0500

    add boildetect

--- boildetect/urls.py
+++ boildetect/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-from .views import Boil_View
+from .views import Boil_View, Boil_View_2
 
 urlpatterns = [
     path("", Boil_View.as_view(), name="boil_detect"),
+    path("b/", Boil_View_2.as_view(), name="boil_detect2"),
 ]
-
-
--- boildetect/views.py
+++ boildetect/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Boil_View(TemplateView):
     template_name = "boildetect/index.html"
+
+class Boil_View_2(TemplateView):
+    template_name = "boildetect/index2.html"
