6f1ae86d32b9b4a3ed5faca03049205de2023a3e
Tropic.Wang <Nemowang24@gmail.com>
2025-03-16 13:06:45 -0400

    allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["192.168.1.240", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["tropicw.net", "192.168.1.240", "localhost", "127.0.0.1"]
 
 # Application definition
 
