24f9c1303b6da115fbfea14d3543fe76367bfa12
Tropic.Wang <Nemowang24@gmail.com>
2025-03-28 22:40:03 -0400

    boil

--- boildetect/urls.py
+++ boildetect/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from .views import Boil_View, Boil_View_2
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path("", Boil_View.as_view(), name="boil_detect"),
+    path("detect", Boil_View.as_view(), name="boil_detect"),
+    path("", TemplateView.as_view(template_name="boildetect/intro.html"), name="boil_intro"),
 ]
