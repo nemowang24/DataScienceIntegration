2a15d72bcf4a456a13e3700c6762721dc834ab83
Tropic.Wang <Nemowang24@gmail.com>
2025-05-18 20:39:27 -0400

    modify s3 bucket

--- pages/views.py
+++ pages/views.py
@@ -111,14 +111,14 @@ class PresignedUrlView(View):
             "s3",
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION,
+            region_name=settings.AWS_S3_REGION_NAME,
         )
 
         try:
             # Generate the presigned URL
             presigned_url = s3_client.generate_presigned_url(
                 "get_object",
-                Params={"Bucket": settings.AWS_S3_BUCKET_NAME, "Key": file_name},
+                Params={"Bucket": settings.AWS_S3_PRIVATE_BUCKET_NAME, "Key": file_name},
                 ExpiresIn=3600,  # URL valid for 1 hour
             )
             return JsonResponse({"presigned_url": presigned_url})
